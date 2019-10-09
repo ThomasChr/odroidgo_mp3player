44d9156da86d5c6288ce05859535831ba0e42381
Thomas Christlieb <thomaschristlieb@hotmail.com>
2019-10-09 20:37:19 +0200

    First crude try to play sound from a raw 8 bit mono sound file

--- mp3player/main/mp3player.c
+++ mp3player/main/mp3player.c
@@ -3,6 +3,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "driver/dac.h"
 #include "sdkconfig.h"
 
 #include "odroid_display.h"
@@ -16,7 +17,7 @@ void mp3playermain()
 {
 	/************ SDCARD ************/
 	odroid_sdcard_open("/sdcard");
-	puts("SDCard init done");	
+	puts("SDCard init done");
 
 	FILE* f = fopen("/sdcard/test.txt", "r");
 	if (f != NULL) {
@@ -29,36 +30,88 @@ void mp3playermain()
 		puts("File test.txt not found");
 	}
 	/************ /SDCARD ************/
-
+	
 	/************ DISPLAY ************/
-	ili9341_init();
-	ili9341_clear(0xffff);
-	puts("LCD init done");
+    uint16_t* buffer;
 
-	uint16_t* buffer;
-	// 320 x 240 Pixel, 16bit each, Color: RRRRRGGG GGGBBBBB (RGB565)
-	buffer = malloc(320*240*2);
-	memset(buffer, 0, 320*240*2);
-	puts("Drawing");
+    /* odroid_display.c */
+    ili9341_init();
+    ili9341_clear(0xffff);
+    puts("Display init done");
 
-	// blue rectangle, 50x50 pixel, left upper corner
-	for (short x = 0; x < 50; x++) {
-		for (short y = 0; y < 50; y++) {
-			buffer[x*320+y] = 0xFF00;
+    // 320 x 240 Pixel, 16bit each, Color: RRRRRGGG GGGBBBBB (RGB565)
+    buffer = malloc(320*240*2);
+    memset(buffer, 0, 320*240*2);
+
+    puts("Drawing");
+    // blue rectangle, 50x50 pixel, left upper corner
+    for (short x = 0; x < 50; x++) {
+        for (short y = 0; y < 50; y++) {
+            buffer[x*320+y] = 0xFF00;
+        }
+    }
+
+    ili9341_write_frame(buffer);
+    puts("Drawing done");
+    /************ /DISPLAY ************/	
+
+	/************ SOUND ************/
+	puts("Preparing sound");
+    dac_channel_t dac_channel1 = DAC_CHANNEL_1;
+	dac_channel_t dac_channel2 = DAC_CHANNEL_2;
+    gpio_num_t dac_gpio1 = 25;
+	gpio_num_t dac_gpio2 = 26;
+
+    gpio_pad_select_gpio(dac_gpio1);
+    gpio_set_direction(dac_gpio1, GPIO_MODE_OUTPUT);
+	gpio_set_level(dac_gpio1, 1);
+
+    gpio_pad_select_gpio(dac_gpio2);
+    gpio_set_direction(dac_gpio2, GPIO_MODE_OUTPUT);
+	gpio_set_level(dac_gpio2, 1);
+
+    dac_output_enable(dac_channel1);
+	dac_output_enable(dac_channel2);
+
+	// differential DAC, we set DAC2 to 0 volt:
+	dac_output_voltage(dac_channel2, 0);
+	
+	// Created with audacity, raw wave file, mono, 8 bit, no header
+	FILE* s = fopen("/sdcard/tetris.raw", "r");
+	char tone;
+	
+	puts("Reading buffer");
+	// read file in 64 KB Buffer, make sure Buffer is in internal RAM
+	int buffersize = 64 * 1024;
+	char *soundfile = heap_caps_malloc(buffersize, MALLOC_CAP_DMA);
+	tone = fgetc(s);
+	int i = 0;
+	while (tone != EOF && i < buffersize) {		
+		soundfile[i++] = tone;
+		tone = fgetc(s);
+	}
+	
+	puts("Starting sound");
+	for (int i; i <= buffersize; i++) {
+		dac_output_voltage(dac_channel1, soundfile[i]);
+		
+		// delay
+		int sleep = 0;
+		while (sleep <= 255) {
+			sleep++;
 		}
 	}
-	ili9341_write_frame(buffer);
-	puts("Drawing done");
-        /************ /DISPLAY ************/
+	puts("Sound done");
+	/************ /SOUND ************/
 
 	/************ MP3LIB ************/
-	
+
 	/************ /MP3LIB ************/
 
-
+	/************ BLINK ************/
 	puts("Starting blink");
-    	// make LED Output
-	gpio_pad_select_gpio(LED); 
+    // make LED Output
+	gpio_pad_select_gpio(LED);
 	gpio_set_direction(LED, GPIO_MODE_OUTPUT);
 
 	while (1) {
@@ -67,11 +120,12 @@ void mp3playermain()
 	        gpio_set_level(LED, 1);
 	        vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
+	/************ /BLINK ************/	
 }
 
 void app_main()
 {
-	// Stacksize is 8 KB (8192)
-    	xTaskCreatePinnedToCore(&mp3playermain, "mp3player_main", 8192, NULL, 5, NULL, 0);
+	// Stacksize is 8 KB (8192), Core is 0
+    xTaskCreatePinnedToCore(&mp3playermain, "mp3player_main", 8192, NULL, 5, NULL, 0);
 }
 
